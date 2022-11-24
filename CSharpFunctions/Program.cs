598ea3605191c33d66b27d6daf49a20e626ad9e0
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-24 15:57:37 +0100

    completata stampa array elevata al quadrato

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//In questo esercizio vi chiedo di definire qualche funzione di utilità che poi potete usare per poter fare operazioni complesse nei vostri programmi principali.
-//void StampaArrayInteri(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]”. Potete prendere quella fatta in classe questa mattina
+// In questo esercizio vi chiedo di definire qualche funzione di utilità che poi potete usare per poter fare operazioni complesse nei vostri programmi principali.
+// void StampaArrayInteri(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]”. Potete prendere quella fatta in classe questa mattina
 
-int [] arreyDiNumeri = { 9, 8, 7, 6, 5, 4};
-StampaArreyInteri(arreyDiNumeri);
+int [] arrayDiNumeri = { 9, 8, 7, 6, 5, 4};
+StampaArreyInteri(arrayDiNumeri);
 
 void StampaArreyInteri (int[] arrayInteri)
 {
@@ -28,7 +28,7 @@ void StampaArreyInteri (int[] arrayInteri)
 Console.WriteLine();
 Console.WriteLine();
 
-//int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
+// int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
 
 Quadrato(10);
 Console.WriteLine(Quadrato(10));
@@ -38,3 +38,25 @@ int Quadrato (int numero)
     int n = (numero * numero);
     return n;
 }
+
+Console.WriteLine();
+
+// int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi elevati quadrato.
+
+StampaArreyInteri(ElevaArrayAlQuadrato(arrayDiNumeri));
+
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] arrayClone = (int[]) array.Clone();
+
+    for (int i = 0; i < arrayClone.Length; i++)
+    {
+        arrayClone[i] = (Quadrato(arrayClone[i]));
+    }
+
+    return arrayClone;
+
+}
+
+Console.WriteLine();
